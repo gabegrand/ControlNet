4306bc278dc66149cfeac98c4c5cb9317979e6b7
Alexis Ross <alexisro@dgx2.csail.mit.edu>
2023-05-08 15:42:09 -0400

    run hyperparams

--- project/run_train.sh
+++ project/run_train.sh
@@ -1,8 +1,8 @@
 export CUDA_VISIBLE_DEVICES=6,7,15
 
-for BATCH in 2 4
+for BATCH in 8
 do
-	for LR in 5e-6 1e-5 5e-5
+	for LR in 1e-6 5e-6 1e-5 5e-5
 	do
 		python train.py --batch_size ${BATCH} --lr ${LR}
 	done
--- project/train.py
+++ project/train.py
@@ -29,6 +29,16 @@ exp_dir = f'./experiments/lr={learning_rate}_bs={batch_size}'
 print("Experiment Directory: ", exp_dir)
 print("Learning Rate: ", learning_rate)
 print("Batch Size: ", batch_size)
+if batch_size >= 4:
+    per_device_batch_size = 4
+    grad_acc_steps = int(batch_size / per_device_batch_size)
+    # TODO: this won't work for batch sizes that aren't multiples of 4
+    assert per_device_batch_size * grad_acc_steps == batch_size
+else:
+    per_device_batch_size = batch_size
+    grad_acc_steps = 1
+print("Per Device Batch Size: ", per_device_batch_size)
+print("Grad Accumulation Steps: ", grad_acc_steps)
 
 # Wandb
 #wandb_logger = pl.loggers.WandbLogger()
@@ -38,14 +48,14 @@ print("Batch Size: ", batch_size)
 print("Creating data...")
 train_dataset = PhotoSketchDataset(split='train')
 val_dataset = PhotoSketchDataset(split='val')
-train_dataloader = DataLoader(train_dataset, num_workers=0, batch_size=batch_size, shuffle=True)
-val_dataloader = DataLoader(val_dataset, num_workers=0, batch_size=batch_size, shuffle=False)
+train_dataloader = DataLoader(train_dataset, num_workers=0, batch_size=per_device_batch_size, shuffle=True)
+val_dataloader = DataLoader(val_dataset, num_workers=0, batch_size=per_device_batch_size, shuffle=False)
 image_logger = ImageLogger(batch_frequency=logger_freq)
 stop_callback = pl.callbacks.early_stopping.EarlyStopping(monitor="val/loss", mode="min", patience=3)
 callbacks = [image_logger, stop_callback]
 #callbacks = [stop_callback]
-#trainer = pl.Trainer(gpus=1, precision=32, callbacks=callbacks, default_root_dir=exp_dir, logger=wandb_logger)
-trainer = pl.Trainer(gpus=3, accelerator="gpu", strategy="ddp", precision=32, callbacks=callbacks, default_root_dir=exp_dir)
+#trainer = pl.Trainer(gpus=1, precision=32, callbacks=callbacks, default_root_dir=exp_dir, accumulate_grad_batches=grad_acc_steps)
+trainer = pl.Trainer(gpus=3, accelerator="gpu", strategy="ddp", precision=32, callbacks=callbacks, default_root_dir=exp_dir, accumulate_grad_batches=grad_acc_steps)
 
 # First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
 print("Creating models...")
